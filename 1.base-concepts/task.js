73b781775235a00719f4a9f849ecafee230726ed
Pavel Nizovtsev <nizo.pavka@yandex.ru>
2022-10-04 22:10:18 +0300

    change task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,19 @@
 function solveEquation(a, b, c) {
-   //    "use strict" - при строгом режиме не выполняется функция;
+   "use strict";
 
    let arr = [];
+   let x, x1, x2;
+
    let d = Math.pow(b, 2) - 4 * a * c;
    //       d- дискриминант, x, x1, x2 - корни уравнения;
    if (d == 0) {
       x = -b / (2 * a);
-      arr.push(x.toFixed(2));
+      arr.push(x);
    }
    if (d > 0) {
       x1 = (-b + Math.sqrt(d)) / (2 * a);
       x2 = (-b - Math.sqrt(d)) / (2 * a);
-      arr.push(x1.toFixed(2), x2.toFixed(2));
+      arr.push(x1, x2);
    }
    if (d < 0) {
       return arr;
