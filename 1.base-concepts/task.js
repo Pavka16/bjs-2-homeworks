c99253d79780e444917284f214c56fe9092200db
Pavel Nizovtsev <nizo.pavka@yandex.ru>
2022-10-04 21:44:34 +0300

    solution of 1 task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,25 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+   //    "use strict" - при строгом режиме не выполняется функция;
+
+   let arr = [];
+   let d = Math.pow(b, 2) - 4 * a * c;
+   //       d- дискриминант, x, x1, x2 - корни уравнения;
+   if (d == 0) {
+      x = -b / (2 * a);
+      arr.push(x.toFixed(2));
+   }
+   if (d > 0) {
+      x1 = (-b + Math.sqrt(d)) / (2 * a);
+      x2 = (-b - Math.sqrt(d)) / (2 * a);
+      arr.push(x1.toFixed(2), x2.toFixed(2));
+   }
+   if (d < 0) {
+      return arr;
+   }
+
+   return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
