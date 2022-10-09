b75779b722a6f7cc5c363f98d8e1a002b7ab0188
Pavel Nizovtsev <nizo.pavka@yandex.ru>
2022-10-09 22:59:04 +0300

    decision task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,24 @@
 function compareArrays(arr1, arr2) {
   let result;
+   if (arr1.length !== arr2.length) {
+      return false;
+   }
+   for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+         return false;
+      }
+   } return true;
 
-  // Ваш код
+   result = every(compareArrays);
 
-  return result; // boolean
+   return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
+   resultArr = arr.filter(elem => elem > 0).
+      filter(elem => elem % 3 == 0).
+      map(elem => elem * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+   return resultArr;
 }
