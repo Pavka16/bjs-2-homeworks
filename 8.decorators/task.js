4e450daa558d977b6ed39b4fde39e8d8b968cc96
Pavel Nizovtsev <nizo.pavka@yandex.ru>
2022-11-06 21:33:13 +0300

    change task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,45 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+   let cache = [];
+
+   function wrapper(...args) {
+      const hash = args.join(',');
+      let objectInCashe = cache.find(object => object.hash === hash);
+      if (objectInCashe) {
+         console.log("Из кэша: " + objectInCashe.result);
+         return "Из кэша: " + objectInCashe.result;
+      }
+
+      let result = func(...args);
+      cache.push({ hash, result });
+      if (cache.length > 5) {
+         cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+   }
+   return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+
+function debounceDecoratorNew(func, ms) {
+   let timerId = null;
+   wrapper.count = 0;
+   wrapper.allCount = 0;
+
+   function wrapper(...args) {
+      wrapper.allCount++;
+
+      if (timerId === null) {
+         func(...args);
+         wrapper.count++;
+      }
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+         wrapper.count++;
+         func(...args);
+      }, ms);
+   }
+   return wrapper;
 }
+
